f536f020d29a319d50c16e2061a43844d60e774a
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2024-02-21 01:36:22 +0200

    fix ejs display on vercel

--- app.js
+++ app.js
@@ -47,9 +47,9 @@ app.use(passport.session());
 
 // Test pages - REMOVE BEFORE DEPLOY
 
-app.set("views", __dirname + "/views");
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("home", { user: req.user });
