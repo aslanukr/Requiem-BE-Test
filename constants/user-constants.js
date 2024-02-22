7ca28df96d50bfa73cd35cfdc962682e63c7ff12
Nataliia Shevchenko <shevchenkonatalua@gmail.com>
2024-02-22 18:08:40 +0200

    update nameRegexp

--- constants/user-constants.js
+++ constants/user-constants.js
@@ -2,7 +2,7 @@ export const emailRegexp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 
 export const passwordRegexp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,16}$/;
 
-export const nameRegexp = /^(?=.{1,40}$)[а-яіїёА-ЯІЇЁ]+(?:[-' ][а-яіїёґА-ЯІЇЁҐ]+)*$/;
+export const nameRegexp = /^(?=.{1,40}$)[а-яіїєґёА-ЯІЄЇЁҐ]+(?:[-' ][а-яіїєґёА-ЯІЄЇЁҐ]+)*$/;
 
 export const phoneRegexp = /^(\+|)[0-9]{10,15}$/;
 
