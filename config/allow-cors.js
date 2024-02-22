b02b529b267b78d2519750052e31178357933d41
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2024-02-23 01:28:26 +0200

    Update allow-cors.js

--- config/allow-cors.js
+++ config/allow-cors.js
@@ -8,7 +8,6 @@ const allowCors = (fn) => async (req, res) => {
     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
   );
   res.setHeader(
-    "Access-Control-Allow-Headers",
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
   );
   if (req.method === "OPTIONS") {
