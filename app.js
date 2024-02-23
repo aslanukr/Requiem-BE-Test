f3c2f85f383cc3169183fda57cef2642371f374d
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2024-02-23 17:52:39 +0200

    sameSite: "strict"

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(
       maxAge: 1000 * 60 * 60 * 24,
       httpOnly: true,
       secure: false,
-      sameSite: "none", //CHANGE BEFORE DEPLOY (because it blocks POST Http requests)
+      sameSite: "strict", //CHANGE BEFORE DEPLOY (because it blocks POST Http requests)
     },
     store: MongoStore.create({
       mongoUrl: DB_HOST,
