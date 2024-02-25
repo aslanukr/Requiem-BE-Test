5dead190e69d1596dbf0de15088ff8937a3e0563
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2024-02-26 00:19:10 +0200

    one of endless attempts to get it work

--- app.js
+++ app.js
@@ -29,9 +29,9 @@ app.use(
     cookie: {
       maxAge: 1000 * 60 * 60 * 24,
       httpOnly: true,
-      // secure: true,
+      secure: false,
       // sameSite: "none", //CHANGE BEFORE DEPLOY (because it blocks POST Http requests)
-      domain: ".vercel.app",
+      // domain: ".vercel.app",
     },
     store: MongoStore.create({
       mongoUrl: DB_HOST,
