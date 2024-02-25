a4e221a8d67b9a5da887ea14a92fae70b65d5902
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2024-02-26 01:30:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -41,9 +41,7 @@ app.use(
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 
-app.use(
-  cors({ credentials: true, origin: "https://requiem-front.vercel.app/" })
-); //CHANGE BEFORE DEPLOY (with origin URL)
+app.use(cors({ credentials: true, origin: true })); //CHANGE BEFORE DEPLOY (with origin URL)
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
