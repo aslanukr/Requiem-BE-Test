954f55f7541ac7b822912a4f5a0c20b8838464cf
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2024-02-25 22:14:42 +0200

    uncomment httponly

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(
     resave: false,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24,
-      // httpOnly: true,
+      httpOnly: true,
       secure: true,
       sameSite: "none", //CHANGE BEFORE DEPLOY (because it blocks POST Http requests)
       domain: ".vercel.app",
