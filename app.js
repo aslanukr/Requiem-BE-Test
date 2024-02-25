1458c8e6beffae37b08dc2755db0bb11d697bd6d
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2024-02-25 22:00:38 +0200

    sameSite none

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(
       maxAge: 1000 * 60 * 60 * 24,
       httpOnly: true,
       secure: false,
-      sameSite: "strict", //CHANGE BEFORE DEPLOY (because it blocks POST Http requests)
+      sameSite: "none", //CHANGE BEFORE DEPLOY (because it blocks POST Http requests)
       domain: ".vercel.app",
     },
     store: MongoStore.create({
