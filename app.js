31f73c4df8262bb31dfc53cd958fa93da2b321af
Nataliia Shevchenko <shevchenkonatalua@gmail.com>
2024-02-27 10:43:09 +0200

    remove headers in cors

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 
 app.use(
-  cors({ credentials: true, origin: 'https://requiem-front.vercel.app', allowedHeaders: "*", exposedHeaders:"*" })
+  cors({ credentials: true, origin: 'https://requiem-front.vercel.app'})
 ); //CHANGE BEFORE DEPLOY (with origin URL)
 app.use(express.json());
 
