ca14da3ec6c192c5018650afc393b710cf4ed7f9
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2024-07-09 22:19:25 +0300

    add cron job

--- controllers/users/testingCron.js
+++
@@ -1,9 +0,0 @@
-import { ctrlWrapper } from "../../decorators/index.js";
-
-const testingCron = (req, res) => {
-  res.json({
-    message: "Cron successfull!",
-  });
-};
-
-export default ctrlWrapper(testingCron);
